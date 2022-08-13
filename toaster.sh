3632ef51733b55206d0a3389d9358c0eeaba4b84
Richard Hildred <rhildred@gmail.com>
2022-08-13 13:16:37 -0400

    remove plugin install as it seems too early

--- toaster.sh
+++ toaster.sh
@@ -1,6 +1,5 @@
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar --info
 php ./wp-cli.phar core download
-php ./wp-cli.phar plugin install simple-xml-sitemap-generator --activate
 curl -o wp-content/db.php https://raw.githubusercontent.com/aaemnnosttv/wp-sqlite-db/master/src/db.php
 curl -O https://raw.githubusercontent.com/rhildred/test_wordpress/main/wp-config.php
