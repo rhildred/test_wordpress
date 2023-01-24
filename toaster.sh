b0694c9e032b303bed118f545d8a75c5348c1ac4
Richard Hildred <rhildred@gmail.com>
2023-01-23 20:21:27 -0500

    add local README

--- toaster.sh
+++ toaster.sh
@@ -3,3 +3,4 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 php ./wp-cli.phar core download
 curl -o wp-content/db.php https://raw.githubusercontent.com/aaemnnosttv/wp-sqlite-db/master/src/db.php
 curl -O https://raw.githubusercontent.com/rhildred/test_wordpress/main/wp-config.php
+curl -o README.md https://raw.githubusercontent.com/rhildred/test_wordpress/main/README.install.md
