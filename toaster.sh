460869f9a6d677e6a611ae5f3f0b0576566412eb
Richard Hildred <rhildred@gmail.com>
2024-01-04 11:40:00 -0500

    remove --info

--- toaster.sh
+++ toaster.sh
@@ -1,5 +1,5 @@
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar --info
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 php ./wp-cli.phar core download
 curl -o wp-content/db.php https://raw.githubusercontent.com/aaemnnosttv/wp-sqlite-db/master/src/db.php
 curl -O https://raw.githubusercontent.com/rhildred/test_wordpress/main/wp-config.php
